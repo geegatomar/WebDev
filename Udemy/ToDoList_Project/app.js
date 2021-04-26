d09c291006fa57c5a6da8e8966f5b94df5c0bd9a
geegatomar <geegatomar@gmail.com>
2021-04-27 00:39:00 +0530

    refactored and added modules and exports

--- Udemy/ToDoList_Project/app.js
+++ Udemy/ToDoList_Project/app.js
@@ -1,24 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static("public"));
+
 app.set("view engine", "ejs");
 
 let nextItems = ["Buy Food", "Cook Food", "Eat Food"];
 
+
 app.get("/", function(req, res){
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    }
-
-    let day = today.toLocaleDateString("en-US", options); 
-
+    let day = date.getDate();
     res.render("list", {kindOfDay : day, itemList: nextItems});
 });
 
